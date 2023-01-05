a8faf4d2865a834472229beb582d0498921a22d6
Prohit <udkarprohit@gmail.com>
2023-01-05 07:55:38 +0530

    Added: Employee Attendance

--- src/com/bridgelabz/EmployeeWage.java
+++ src/com/bridgelabz/EmployeeWage.java
@@ -1,7 +1,17 @@
 package com.bridgelabz;
 
+
+
 public class EmployeeWage {
     public static void main(String[] args) {
         System.out.println("Welcome to employee wage");
+        int ran = (int)( Math.random() * 10 )% 2;
+        //System.out.println(ran);
+        if(ran == 0)
+        {
+            System.out.println("Employee is Present");
+        }else {
+            System.out.println("Employee is Absent");
+        }
     }
 }
